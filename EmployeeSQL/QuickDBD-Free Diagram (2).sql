4f105b01eb6cca1fc9bdd802f31ffa58eabd5493
laurar9889 <laurar9889@hotmail.com>
2024-04-29 00:05:14 -0400

    Assignment: - Modified date format to varchar - Modified query to successfully import CSV files

--- EmployeeSQL/QuickDBD-Free Diagram (2).sql
+++ EmployeeSQL/QuickDBD-Free Diagram (2).sql
@@ -6,74 +6,88 @@
 -- To reset the sample schema, replace everything with
 -- two dots ('..' - without quotes).
 
+--Create Departments table with 1 primary key
+	--Drop table if already exists
+	DROP TABLE IF EXISTS "Departments" CASCADE;
+
 CREATE TABLE "Departments" (
     "dept_no" VARCHAR(4)   NOT NULL,
     "dept_name" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "dept_no"
-     )
+    PRIMARY KEY ("dept_no")
 );
 
+--Create Departments Employee table with 1 primary key
+	--Drop table if already exists
+	--Create a composed primary keys when 2 columns are not unique
+	DROP TABLE IF EXISTS "Dept_emp" CASCADE;
 CREATE TABLE "Dept_emp" (
     "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(4)   NOT NULL,
-    CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
-        "emp_no","dept_no"
-     )
+    "dept_no" VARCHAR(4) NOT NULL REFERENCES "Departments" ("dept_no"),
+	"emp_title_id" VARCHAR(5) NOT NULL,
+	FOREIGN KEY ("emp_no", "emp_title_id") REFERENCES "Employees"("emp_no","emp_title_id"),
+    PRIMARY KEY ("emp_no","dept_no")
 );
+	--Drop the Composed primary key
+	ALTER TABLE "Dept_emp" DROP column "emp_title_id";
 
+
+--Create Departments Manager table with 2 primary key
+	--Drop table if already exists 
+	--Create a composed primary keys when 2 columns are not unique
+	DROP TABLE IF EXISTS "Dept_manager" CASCADE;
 CREATE TABLE "Dept_manager" (
-    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL REFERENCES "Departments"("dept_no"),
     "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
-        "dept_no","emp_no"
-     )
-);
+	"emp_title_id" VARCHAR(5) NOT NULL,
+	FOREIGN KEY ("emp_no", "emp_title_id") REFERENCES "Employees"("emp_no","emp_title_id"),
+ 	PRIMARY KEY ("dept_no","emp_no")
+	);
+	--Drop the Composed primary key
+	ALTER TABLE "Dept_manager" DROP column "emp_title_id";
 
+
+--Create Title table with 2 primary key
+	--Drop table if already exists 
+	--Create a composed primary keys when 2 columns are not unique
+	DROP TABLE IF EXISTS "Titles";
 CREATE TABLE "Titles" (
-    "title_id" VARCHAR(5)   NOT NULL,
+    "emp_title_id" VARCHAR(5)   NOT NULL,
     "title" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "title_id"
-     )
+	"emp_no" INT NOT NULL,
+	FOREIGN KEY("emp_no","emp_title_id") REFERENCES "Employees" ("emp_no","emp_title_id"),
+    PRIMARY KEY ("emp_title_id","title")
 );
+	--Drop the Composed primary key
+	ALTER TABLE "Titles" DROP column "emp_no";
+
+
+
+--Create Salaries table with 2 primary keys
+	--Drop table if already exists
+	DROP TABLE IF EXISTS "Salaries" CASCADE;
 
 CREATE TABLE "Salaries" (
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL
+    "emp_no" INT NOT NULL,
+	"emp_title_id" VARCHAR (5) NOT NULL,
+    "salary" INT   NOT NULL,
+	FOREIGN KEY ("emp_no", "emp_title_id") REFERENCES "Employees"("emp_no", "emp_title_id"),
+	PRIMARY KEY ("emp_no")
 );
+	--Drop the Composed primary key
+	ALTER TABLE "Salaries" DROP column "emp_title_id";
+
+
+--Create Employee table with 2 primary keys
+	--Drop table if already exists
+	DROP TABLE IF EXISTS "Employees" CASCADE;
 
 CREATE TABLE "Employees" (
     "emp_no" INT   NOT NULL,
     "emp_title_id" VARCHAR(5)   NOT NULL,
-    "birth_date" VARCHAR(10)   NOT NULL,
+    "birth_date" VARCHAR(10) NOT NULL,
     "first_name" VARCHAR(50)   NOT NULL,
     "last_name" VARCHAR(50)   NOT NULL,
     "sex" VARCHAR(1)   NOT NULL,
     "hire_date" VARCHAR(10)   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-ALTER TABLE "Departments" ADD CONSTRAINT "fk_Departments_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Dept_manager" ("dept_no");
-
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
-
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
-
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
-
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Departments_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
-
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
-
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "Titles" ("title_id");
-
+    PRIMARY KEY ("emp_no", "emp_title_id")
+	);
