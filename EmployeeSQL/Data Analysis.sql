ae00bdb98d8f365e63b142756581524bc44497e4
laurar9889 <laurar9889@hotmail.com>
2024-04-30 22:21:57 -0400

    Assignment: take out "" when not needed

--- EmployeeSQL/Data Analysis.sql
+++ EmployeeSQL/Data Analysis.sql
@@ -1,12 +1,16 @@
---1.  LIST OF EMPLOYEE NUMBER, LAS & FIRST NAME, SEX, SALARY
-SELECT "first_name", "last_name", "sex", "hire_date"
+
+--1.  List employee number, last and first name, sex and salary.
+
+SELECT "Employees".emp_no, "Employees".first_name, "Employees".last_name, "Employees".sex, "Salaries".salary
 FROM "Employees"
 JOIN "Salaries"
-ON "Employees"."emp_no" = "Salaries"."emp_no";
+ON "Employees".emp_no = "Salaries".emp_no;
 
---2. LIST FIRST AND LAST NAME, HIRE DATE FOR EMPLOYEES HIRED IN 1986
+--2. List first and last name,  hire date for employees hired in 1986
 --Source: https://www.postgresqltutorial.com/postgresql-date-functions/postgresql-to_date/
--- change hire date and birth date format, to date
+-- change hire date and birth date format, to date (with the help of tutor Brandon Wong)
+-- 2.A. create a view with the 2 additional columns for hire date and birth date.
+DROP VIEW IF EXISTS employee_view;
 
 CREATE VIEW employee_view AS 
 SELECT *, TO_DATE ("hire_date", 'MM/DD/YYYY') AS hire_date_format,
@@ -15,12 +19,12 @@ FROM "Employees";
 
 SELECT * FROM employee_view;
 
---2. LIST FIRST AND LAST NAME, HIRE DATE FOR EMPLOYEES HIRED IN 1986
+--2.B. List first and last name, hire date for emplyees hired in 1986
 SELECT first_name, last_name, hire_date_format
 FROM employee_view
-WHERE hire_date_format > '1986-1-1' AND hire_date_format <'1987-1-1';
+WHERE hire_date_format >= '1986-1-1' AND hire_date_format <'1987-1-1';
 
---3. list manager of each department along with their department number, department name,
+--3. List manager of each department along with their department number, department name,
 --employee number, last name, first name.
 SELECT "Dept_manager"."dept_no", "Departments"."dept_name", "Employees"."emp_no", "Employees"."last_name", "Employees"."first_name"
 FROM "Dept_manager"
@@ -29,11 +33,42 @@ ON "Dept_manager"."emp_no" = "Employees"."emp_no"
 JOIN "Departments"
 ON "Dept_manager"."dept_no" = "Departments"."dept_no";
 
-
---4. List department number for each employee, employee #, last name andfirst name, department name.
-SELECT "Dept_emp"."dept_no", "Employees"."emp_title_id", "Employees"."last_name", "Employees"."first_name", "Departments"."dept_name"
+--4. List department number for each employee, employee #, last name and first name, department name.
+SELECT "Dept_emp"."dept_no", "Employees"."emp_no", "Employees"."last_name", "Employees"."first_name", "Departments"."dept_name"
 FROM "Employees"
 JOIN "Dept_emp"
 ON "Employees"."emp_no" = "Dept_emp"."emp_no"
 JOIN "Departments"
 ON "Dept_emp"."dept_no" = "Departments"."dept_no";
+
+--5. List first name, last mane, sex of each employee whose first name is Hercules and last name begins with "B" 
+SELECT first_name, last_name, sex
+FROM "Employees"
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--6. List each employee in the Sales Department, including employee #, last and first name, and department name.
+SELECT "Dept_emp".dept_no, "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Departments".dept_name
+FROM "Employees"
+JOIN "Dept_emp"
+ON "Employees".emp_no = "Dept_emp".emp_no
+JOIN "Departments"
+ON "Dept_emp".dept_no = "Departments".dept_no
+WHERE "Departments".dept_name = 'Sales';
+
+--7. List each employee in the Sales and Development departments, including employee #, last and first name, and department name.
+SELECT "Dept_emp".dept_no, "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Departments".dept_name
+FROM "Employees"
+JOIN "Dept_emp"
+ON "Employees".emp_no = "Dept_emp".emp_no
+JOIN "Departments"
+ON "Dept_emp".dept_no = "Departments".dept_no
+WHERE "Departments".dept_name = 'Sales' OR "Departments".dept_name ='Development';
+
+--8. List frecuency counts, descending order of all employee last names.
+--Source: https://www.w3resource.com/sql/aggregate-functions/count-with-group-by.php
+SELECT "Employees".last_name, COUNT(*)
+FROM "Employees"
+GROUP BY "Employees".last_name
+ORDER BY COUNT(*) DESC;
+
+
--- EmployeeSQL/QuickDBD-Free Diagram (2).sql
+++ EmployeeSQL/QuickDBD-Free Diagram (2).sql
@@ -1,53 +1,50 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/paPbon
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 -- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
-
 
 
 --1. Create Departments table with 1 primary key
+-- CASCADES helps have all tables up-to-date (parent and child)
 	--Drop table if already exists
 	DROP TABLE IF EXISTS "Departments" CASCADE;
 
 CREATE TABLE "Departments" (
-    "dept_no" VARCHAR(4)   NOT NULL,
-    "dept_name" VARCHAR(30)   NOT NULL,
-    PRIMARY KEY ("dept_no")
+    dept_no VARCHAR(4)   NOT NULL,
+    dept_name VARCHAR(30)   NOT NULL,
+    PRIMARY KEY (dept_no)
 );
 
 
---2. Create Employee table with 2 primary keys
+--2. Create Employees table with 2 primary keys
 	--Drop table if already exists
 	DROP TABLE IF EXISTS "Employees" CASCADE;
 
 CREATE TABLE "Employees" (
-    "emp_no" INT   NOT NULL,
-    "emp_title_id" VARCHAR(5)   NOT NULL,
-    "birth_date" VARCHAR(10) NOT NULL,
-    "first_name" VARCHAR(50)   NOT NULL,
-    "last_name" VARCHAR(50)   NOT NULL,
-    "sex" VARCHAR(1)   NOT NULL,
-    "hire_date" VARCHAR(10)   NOT NULL,
-    PRIMARY KEY ("emp_no", "emp_title_id")
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR(5)   NOT NULL,
+    birth_date VARCHAR(10) NOT NULL,
+    first_name VARCHAR(50)   NOT NULL,
+    last_name VARCHAR(50)   NOT NULL,
+    sex VARCHAR(1)   NOT NULL,
+    hire_date VARCHAR(10)   NOT NULL,
+    PRIMARY KEY (emp_no, emp_title_id)
 	);
 	
---3. Create Title table with 1 primary key
+--3. Create Title table with 2 primary key, 1 Foreign key
 	--Drop table if already exists 
 	--Create a composed primary keys when 2 columns are not unique
 	DROP TABLE IF EXISTS "Titles" CASCADE;
 CREATE TABLE "Titles" (
-    "emp_title_id" VARCHAR(5)   NOT NULL,
-    "title" VARCHAR(30)   NOT NULL,
-	"emp_no" INT NOT NULL,
-	FOREIGN KEY("emp_title_id","emp_no") REFERENCES "Employees" ("emp_title_id","emp_no"),
-    PRIMARY KEY ("emp_title_id","title")
+    emp_title_id VARCHAR(5)   NOT NULL,
+    title VARCHAR(30)   NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY(emp_title_id,emp_no) REFERENCES "Employees" (emp_title_id, emp_no),
+    PRIMARY KEY (emp_title_id,title)
 );
 
 	--Drop the Composed primary key
-	ALTER TABLE "Titles" DROP column "emp_no";
+	ALTER TABLE "Titles" DROP column emp_no;
 
 
 --4. Create Salaries table with 2 primary keys
@@ -55,29 +52,30 @@ CREATE TABLE "Titles" (
 	DROP TABLE IF EXISTS "Salaries" CASCADE;
 
 CREATE TABLE "Salaries" (
-    "emp_no" INT NOT NULL,
-	"emp_title_id" VARCHAR (5) NOT NULL,
-    "salary" INT   NOT NULL,
-	FOREIGN KEY ("emp_no", "emp_title_id") REFERENCES "Employees"("emp_no", "emp_title_id"),
-	PRIMARY KEY ("emp_no")
+    emp_no INT NOT NULL,
+	emp_title_id VARCHAR (5) NOT NULL,
+    salary INT   NOT NULL,
+	FOREIGN KEY (emp_no, emp_title_id) REFERENCES "Employees"(emp_no, emp_title_id),
+	PRIMARY KEY (emp_no)
 );
 	--Drop the Composed primary key
-	ALTER TABLE "Salaries" DROP column "emp_title_id";
+	ALTER TABLE "Salaries" DROP column emp_title_id;
 	
 	
---5. Create Departments Employee table with 1 primary key
+--5. Create Departments Employee table with 2 primary key, 2 foreign key
 	--Drop table if already exists
 	--Create a composed primary keys when 2 columns are not unique
 	DROP TABLE IF EXISTS "Dept_emp" CASCADE;
+
 CREATE TABLE "Dept_emp" (
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(4) NOT NULL REFERENCES "Departments" ("dept_no"),
-	"emp_title_id" VARCHAR(5) NOT NULL,
-	FOREIGN KEY ("emp_no", "emp_title_id") REFERENCES "Employees"("emp_no","emp_title_id"),
-    PRIMARY KEY ("emp_no","dept_no")
+    emp_no INT   NOT NULL,
+    dept_no VARCHAR(4) NOT NULL REFERENCES "Departments" (dept_no),
+	emp_title_id VARCHAR(5) NOT NULL,
+	FOREIGN KEY (emp_no, emp_title_id) REFERENCES "Employees"(emp_no, emp_title_id),
+    PRIMARY KEY (emp_no,dept_no)
 );
 	--Drop the Composed primary key
-	ALTER TABLE "Dept_emp" DROP column "emp_title_id";
+	ALTER TABLE "Dept_emp" DROP column emp_title_id;
 
 
 --6. Create Departments Manager table with 2 primary key
@@ -85,13 +83,13 @@ CREATE TABLE "Dept_emp" (
 	--Create a composed primary keys when 2 columns are not unique
 	DROP TABLE IF EXISTS "Dept_manager" CASCADE;
 CREATE TABLE "Dept_manager" (
-    "dept_no" VARCHAR(4)   NOT NULL REFERENCES "Departments"("dept_no"),
-    "emp_no" INT   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL REFERENCES "Departments"("dept_no"),
+    emp_no INT   NOT NULL,
 	"emp_title_id" VARCHAR(5) NOT NULL,
-	FOREIGN KEY ("emp_no", "emp_title_id") REFERENCES "Employees"("emp_no","emp_title_id"),
- 	PRIMARY KEY ("dept_no","emp_no")
+	FOREIGN KEY (emp_no, emp_title_id) REFERENCES "Employees"(emp_no,emp_title_id),
+ 	PRIMARY KEY (dept_no, emp_no)
 	);
 	--Drop the Composed primary key
-	ALTER TABLE "Dept_manager" DROP column "emp_title_id";
+	ALTER TABLE "Dept_manager" DROP column emp_title_id;
 
 
